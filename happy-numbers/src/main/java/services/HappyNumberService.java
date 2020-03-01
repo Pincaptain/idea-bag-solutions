bb6cdc5eb7805c54b98f9a86739d7cb518c73c1b
Borjan Gjorovski <borjan_blizz@hotmail.com>
2020-03-01 02:07:09 +0100

    Remove redundancy from happy-numbers

--- happy-numbers/src/main/java/services/HappyNumberService.java
+++ happy-numbers/src/main/java/services/HappyNumberService.java
@@ -1,10 +1,8 @@
 package services;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class HappyNumberService implements IHappyNumberService {
     @Override
--- happy-numbers/src/main/java/services/IHappyNumberService.java
+++ happy-numbers/src/main/java/services/IHappyNumberService.java
@@ -3,5 +3,5 @@ package services;
 import java.util.List;
 
 public interface IHappyNumberService {
-    public List<Integer> getHappyNumbers(int offset, final int limit);
+    List<Integer> getHappyNumbers(int offset, final int limit);
 }
